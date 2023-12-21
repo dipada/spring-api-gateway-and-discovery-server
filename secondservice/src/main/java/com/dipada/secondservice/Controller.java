f4b9c930934a8dffcc17e476517be1898b3be0f8
dipada <daniele.dipalma68@gmail.com>
2023-12-21 13:41:14 +0100

    added second service

---
+++ secondservice/src/main/java/com/dipada/secondservice/Controller.java
@@ -0,0 +1,14 @@
+package com.dipada.secondservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secondservice")
+public class Controller {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the second service";
+    }
+}
---
+++ secondservice/src/test/java/com/dipada/secondservice/SecondserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipada.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

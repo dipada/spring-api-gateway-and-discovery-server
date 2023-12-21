98d2de6b19d4bd409c32f2c420d2ce5635fb5061
dipada <daniele.dipalma68@gmail.com>
2023-12-21 16:48:08 +0100

    changed default zones

--- secondservice/src/test/java/com/dipada/secondservice/SecondserviceApplicationTests.java
+++ secondservice/src/test/java/com/dipada/secondservice/SecondserviceApplicationTests.java
@@ -2,8 +2,10 @@ package com.dipada.secondservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootTest
+@EnableDiscoveryClient
 class SecondserviceApplicationTests {
 
     @Test
